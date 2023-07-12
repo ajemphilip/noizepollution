bcf3af973204df22b0462bb6cf5354d75cbe7878
Ajem Philip <56880827+ajemphilip@users.noreply.github.com>
2023-07-12 11:01:06 -0400

    git

--- src/LoadingScreen.jsx
+++ src/LoadingScreen.jsx
@@ -33,6 +33,7 @@ opacity:0;
 border: 2px solid white;
 font-size:16px;
 color:white;
+margin-top:3rem;
 transition: all 1s ease;
 transition-delay: 1250ms;
 &.showButton {
@@ -81,13 +82,32 @@ opacity:1;
 }
 `
 
+const War = styled.p`
+font-size: 16px;
+max-width:900px;
+color:white;
+letter-spacing: 0.05rem;
+line-height: 1;
+margin:1rem;
+opacity:0;
+font-weight: 300;
+text-align:center;
+transition: all 1s ease;
+transition-delay: 1250ms;
+&.completed {
+opacity:1;
+}
+`
+
 const LoadingScreen = (props) => {
     const {progress} = useProgress()
     return(
     <Container className={`${progress >= 100 ? "loaded" : ""} ${props.hideLoader ? "start" : ""}`}>
     {!props.hideLoader && <Header1 className={`${progress >= 100 ? "completed" : ""}`}>{progress}</Header1>}
     <Header1Anim className={`${progress >= 100 ? "completed" : ""}`}>Hear The Noize</Header1Anim>
-    <ParDesc className={`${progress >= 100 ? "completed" : ""}`}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto corrupti quas voluptates quidem accusantium aliquam quia laudantium magnam. Cum, suscipit. Quaerat eos quisquam obcaecati debitis enim sequi omnis aliquid voluptatem!</ParDesc>
+    <ParDesc className={`${progress >= 100 ? "completed" : ""}`}>Environmental noise pollution is a crucial topic in modern times. Exposure to noise pollution, just like air or water pollution has a negative impact on human health with disorders such as sleep, cardiovascular, and mental health disturbances. The human brain is exposed to constant and multiple environmental noises and ignores the insignificant portions to provide only the most essential stimulus. Constant noise exposure can lead to mild symptoms of cognitive disorders, and increased brain activity up to severe and acute illnesses. The technique presented - sound walk - efficiently presents city noise but also presents the intensity of environmental sounds processed by the human brain.</ParDesc>
+    <War className={`${progress >= 100 ? "completed" : ""}`}><b>Use Headphones for the Best Experience </b></War>
+    <War className={`${progress >= 100 ? "completed" : ""}`}>Warning -Hign noise levels and fast moving images</War>
     <Button className={`${progress >= 100 ? "showButton" : ""}`} onClick={props.hideLoaderFunction}>Hear The Journey</Button>
     </Container>)
 }
